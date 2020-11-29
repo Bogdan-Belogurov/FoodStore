1f5712995af77d7b46563d11611c06bba61d680e
Bogdan Belogurov <bogdan.belogurov@emlid.com>
2020-11-30 01:07:15 +0300

    Update App function component
    - Add `dataSource`, `items`, `itemList`, `setItems`, `onAdd` and `onDelete` properties
    - Combine all react components and bind with `Router`
    
    Co-authored-by: Polina Dyachkova <dyachkova.polina.a@gmail.com>

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,53 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react'
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
+import ItemDetail from './components/ItemDetail'
+import ItemList from './components/ItemList'
+import Navbar from './components/Navbar'
+import ShoppingCart from './components/ShoppingCart'
+import { IItem } from './interfaces'
+import DataSource from './DataSource'
+import { Order } from './components/Order'
 
-function App() {
+const App: React.FunctionComponent = () => {
+  const [items, setItems] = useState<IItem[]>([])
+  const dataSource = new DataSource()
+  const itemList: Array<IItem> = dataSource.availableItems
+
+  const onAdd = (item: IItem) => {
+    setItems(prev => [item, ...prev])
+  }
+
+  const onDelete = (id: string) => {
+    setItems(prev => prev.filter(item => item.id !== id))
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <>
+      <Router>
+        <Navbar itemsCount={items.length} />
+        <div className="container">
+
+          <Switch>
+            <Route exact path="/">
+              <ItemList items={itemList}></ItemList>
+            </Route>
+            <Route
+              path="/detail/:id"
+              render={props =>
+                <ItemDetail {...props} onAdd={onAdd} />
+              }
+            />
+            <Route path="/shoppingcart">
+              <ShoppingCart items={items} onDelete={onDelete}/>
+            </Route>
+
+            <Route path="/order" component={Order}></Route>
+          </Switch>
+
+        </div>
+      </Router>
+    </>
+  )
 }
 
 export default App;
